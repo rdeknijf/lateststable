273a023654ef3e06a090d981a81b044e4b2edaf7
Rutger de Knijf <rutger@deknijf.com>
2020-10-21 16:04:39 +0200

    remove old deploy script

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-pipenv lock --requirements > requirements.txt
-docker build -t eu.gcr.io/orca-205614/lst:latest .
-docker push eu.gcr.io/orca-205614/lst:latest
-gcloud run deploy lst --image eu.gcr.io/orca-205614/lst:latest --platform managed --allow-unauthenticated --region=europe-west1
