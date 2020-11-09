b81e2c03bb4c11c0da997090806017a649ae42d1
Rutger de Knijf <rutger@deknijf.com>
2020-11-09 17:25:04 +0100

    add an ugly homepage

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
+from pathlib import Path
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from routers import router
 
 app = FastAPI()
 
 app.include_router(router, prefix='/v1')
+
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+async def homepage():
+    with open(Path(Path(__file__).parent, 'homepage.html'), 'r') as f:
+        return f.read()
 
 # for debugging
 if __name__ == '__main__':
